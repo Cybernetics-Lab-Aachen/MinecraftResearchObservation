d11496c218b42c53235c1bad4c15fe8bf0a5ba76
Thorsten Sommer <service@sommer-engineering.com>
2015-07-19 18:12:50 +0200

    Manage the versions

--- MinecraftResearchObservation/Properties/AssemblyInfo.cs
+++ MinecraftResearchObservation/Properties/AssemblyInfo.cs
@@ -1,31 +1,29 @@
-﻿#region Using directives
-
+#region Using directives
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 #endregion
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MinecraftResearchObservation")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MinecraftResearchObservation")]
-[assembly: AssemblyCopyright("Copyright 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
+[assembly: AssemblyTitle ("MinecraftResearchObservation")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("MinecraftResearchObservation")]
+[assembly: AssemblyCopyright ("Copyright 2015")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
-[assembly: ComVisible(false)]
-
+[assembly: ComVisible (false)]
 // The assembly version has following format :
 //
 // Major.Minor.Build.Revision
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion ("2")]
+[assembly: AssemblyFileVersion ("2")]
+[assembly: Guid ("d0acb3ef-e2a4-4991-be16-15d5bb3f6e54")]
